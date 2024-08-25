a365f46498c5275521cf5a6579192b03be7abe26
Michael Moffitt <mikejmoffitt@gmail.com>
2024-08-26 07:59:16 +0900

    xbase uses gawk explicitly to fix MacOS compat

--- xb-rules.mk
+++ xb-rules.mk
@@ -73,7 +73,7 @@ $(OBJDIR)/%.o: %.c $(XSP2LIBDIR) $(SOURCES_H)
 $(OBJDIR)/%.o: %.a68 $(XSP2LIBDIR) $(SOURCES_H)
 	@mkdir -p $(OBJDIR)/$(<D)
 	@bash -c 'printf "\t\e[95m[ ASM ]\e[0m $<\n"'
-	awk '{gsub(/;/,";#"); printf("%s", $$0 RT)}' RS='"[^"]*"' $< | awk '{gsub(/\$$/,"0x"); printf("%s", $$0 RT)}' RS='"[^"]*"' | $(AS) $(ASFLAGS) -o $@ -c -
+	gawk '{gsub(/;/,";#"); printf("%s", $$0 RT)}' RS='"[^"]*"' $< | gawk '{gsub(/\$$/,"0x"); printf("%s", $$0 RT)}' RS='"[^"]*"' | $(AS) $(ASFLAGS) -o $@ -c -
 
 clean:
 	$(RM) $(OBJECTS_C) $(OBJECTS_ASM)
